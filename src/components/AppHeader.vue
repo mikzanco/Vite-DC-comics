8144cf504a5ba850779b83fce2b0206c950e6fce
mikzanco <mikyzancolo97@gmail.com>
2022-11-15 16:25:18 +0100

    fatto l'header della pagina

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,9 +6,26 @@ export default {
 
 <template>
   <header>
-    <div>
+    <div class="container">
+      <div>
       <img src="/dc-logo.png" alt="">
     </div>
+    <nav>
+      <ul>
+        <li><a href="#">Character</a></li>
+        <li><a href="#">Comics</a></li>
+        <li><a href="#">Movies</a></li>
+        <li><a href="#">Tv</a></li>
+        <li><a href="#">Games</a></li>
+        <li><a href="#">Collectibles</a></li>
+        <li><a href="#">Videos</a></li>
+        <li><a href="#">Fans</a></li>
+        <li><a href="#">News</a></li>
+        <li><a href="#">Shop</a></li>
+      </ul>
+    </nav>
+    </div>
+    
   </header>
 </template>
 
@@ -19,5 +36,21 @@ export default {
     height: 95px;
     display: flex;
     width: 100%;
+    justify-content: center;
+    .container{
+      justify-content: space-around;
+      img{
+      height: 70px;         
+      }
+      ul{
+        display: flex;
+        list-style: none;
+        a{
+          text-decoration: none;
+          padding: 10px;
+        }
+      }
+    }
+    
   }
 </style>
